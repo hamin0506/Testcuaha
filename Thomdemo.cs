2dcd33c737a1ef654dd0261954c2112e2c61b107
Thơmn Đinh <dinhthithom09072005@gmail.com>
2025-09-18 22:59:10 +0700

    Revert "second commit"
    This reverts commit b48133c7a33c4c5d13b9966b1b6c2d1f99fee9d5.

--- Thomdemo.cs
+++ Thomdemo.cs
@@ -17,7 +17,7 @@ namespace Testcuaha
             InitializeComponent();
         }
 
-        private void lbl_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
